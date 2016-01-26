66c0f4c807d5faa6497391181dacdf2a1414df88
Theodoros Ploumis <me@theodorosploumis.com>
2016-01-26 13:44:13 +0200

    Better lat,lon for the White tower, pager limit

--- apps/leaflet-points/script.js
+++ apps/leaflet-points/script.js
@@ -2,10 +2,10 @@
 
   $(document).ready(function(){
 
-    // The main api url for Stories
-    var $url = 'http://manystoriesoneheart.gr/api/v1.0/stories?filter[postal_address][value]=0&filter[postal_address][operator]=%22%3E%22';
+    // The main api url for Stories. Get the latest 30 Stories that have an address.
+    var $url = 'http://manystoriesoneheart.gr/api/v1.0/stories?filter[postal_address][value]=0&filter[postal_address][operator]=%22%3E%22&range=30';
     // The White tower lot/lan point
-    var $thessaloniki = [40.6264251, 22.8783858];
+    var $thessaloniki = [40.6171048, 22.9594983];
     // Initialize empty array of points
     var $points = [];
 
@@ -13,7 +13,7 @@
     var map = L.map("map");
     L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png").addTo(map);
 
-    // Set the view to Thessaloniki point
+    // Set the view to Thessaloniki point (constant)
     map.setView($thessaloniki, 15);
 
     // Get data from API with ajax
